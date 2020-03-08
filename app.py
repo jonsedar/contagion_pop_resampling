17b3d9609ffc9fae9fa325acff51e75dd9a06b9e
Jonathan Sedar <jon.sedar@applied.ai>
2020-03-08 18:52:02 -0400

    reformatting to make easier

--- app.py
+++ app.py
@@ -2,54 +2,79 @@ import streamlit as st
 
 def main():
     st.markdown("# Contagious Coughs & Colds 2019 Commute Calculator")
-    st.markdown("""An under-researched, likely invalid, statistical toy to 
-                illustrate the power of social separation (and conversely the 
-                power of our immune systems) by estimating the 
+    st.markdown("""An under-researched, probably invalid, statistical toy \** to 
+                illustrate the power of social separation by estimating the 
                 probability of encountering someone with a contagious cough or 
-                cold during your daily round trip commute into the city.
-                This makes no attempt to model transmission vectors, nor
-                crowd dynamics, nor self quarantine etc.
-                **Do not use this for anything!**""")
+                cold during your daily round trip commute into the city.""")
 
-    st.markdown("""The probability P(X>0) of a finding at least 1 case in a 
-                group of N people given a background number of cases (m) in the 
-                local city population (M) is a resample with replacement 
-                (m >> 1)""")
+    st.markdown('### A naive population resampling model')
 
-    st.latex(r'P(X>0) = 1 - (1-p)^{N} = f, \, \text{ ... where } \, p = \frac{m}{M}')
+    st.markdown("""The probability $P(X>0)$ of a finding at least $1$ case in a 
+                group of $N$ people given a background number of cases $m$ in the 
+                local city population $M$ is a resample with replacement 
+                $(m >> 1)$: """)
+
+    st.sidebar.markdown('## Interactive Settings')
+
+    st.sidebar.markdown('### Confirmed Cases')
+
+    m = st.sidebar.slider('Count of live cases (m) in the city', min_value=10, 
+                            max_value=5000, value=100, step=50)
+    M = st.sidebar.slider('Population in the city (M)', min_value=500000, 
+                            max_value=20000000, value=4000000, step=500000)
+
+    p = m/M
+    st.sidebar.markdown(f'Cases in population $p = {p*100:.3f}\%$')
+
+    st.sidebar.markdown('### Commute Details')
 
     commute_people_per_min = {'Walk / Cycle': 1.,
                              'Ferry': 4.,
                             'Train / Subway (direct)': 8., 
                             'Train / Subway (1+ change at a busy station)': 12.}
 
-    commute_method = st.radio('Commute Method', 
+    commute_method = st.sidebar.radio('Commute Method', 
                             list(commute_people_per_min.keys()), index=2)
     n_commuters = commute_people_per_min[commute_method]
 
-    st.write(f"""This assumes you come into reasonably close aeresol contact 
-                (d < 2m) with an average 
-                N = {n_commuters:.0f} new commuters per min over the entire 
-                commute, incl. busy stations, crowded streets etc.""")
+    st.sidebar.markdown(f"""Assumes aerosol contact ($\Delta \ 2m$) with 
+                ${n_commuters:.0f}$ new commuters per min over the
+                entire commute, incl. busy stations, crowded streets etc.""")
 
-    commute_duration = st.slider('Commute Duration (mins)', min_value=15, 
+    commute_duration = st.sidebar.slider('Commute Duration (mins)', min_value=15, 
     max_value=120, value=45, step=15)
 
     N = commute_people_per_min[commute_method] * commute_duration * 2
-    st.write(f'N = approx {N:.0f} commuters encountered during a round trip commute')
 
-    m = st.slider('Count of live cases (m) in the city', min_value=10, 
-                            max_value=5000, value=100, step=50)
-    M = st.slider('Population in the city (M)', min_value=500000, 
-                            max_value=20000000, value=4000000, step=500000)
+    st.sidebar.markdown(f'$N \\approx$ {N:.0f} commuters encountered during ' + 
+                        'a round trip commute')
 
-    f = 1 - (1-(m/M))**N
+    st.sidebar.markdown("Now view your $P(X>0) \\Longrightarrow$")
 
-    st.write(f"""Est. prob. P(X>0) of encountering a contagious cough or cold during 
-             your daily round trip commute is: {f:.1%}""")
 
-    st.write(f"""... and over a 5 day week P(X>0|5 days) = 1 - (1-f)^5 is: 
-            {1 - (1-f)**5:.1%}""")
+    st.latex(r'P(X>0) = 1 - (1-p)^{N} = f, \, \text{ ... where } \, p = \frac{m}{M}')
+
+    st.markdown("""$\\Longleftarrow$ Set the sliders on the left according to your city's
+                infection rate and your commute type and duration ...""")
+
+
+    f = 1 - (1-(p))**N
+
+    st.markdown('#### Results:')
+
+    st.info(f"""Est. prob. of encountering a contagious cough or cold during 
+             your daily round trip commute is: $P(X>0) = {f*100:.1f}\\%$""")
+    
+    f5 = 1 - (1-f)**5
+    days= 'days'
+
+    st.info(f"""... and over a 5 day week: $P(X>0|5 \, days) 
+                = 1 - (1-f)^5 = {f5*100:.1f}\\%$""")
+
+    
+    st.markdown("""\** NOTE: This naive model makes no attempt to model 
+                transmission vectors, nor crowd dynamics, nor self quarantine etc.
+                **Do not use this for anything!**""")
 
 
 
