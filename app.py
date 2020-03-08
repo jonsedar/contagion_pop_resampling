6ff2e258d4f5fbb35ac9491d760797aa76bcaf6a
Jonathan Sedar <jon.sedar@applied.ai>
2020-03-08 19:54:05 -0400

    formatting

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
 def main():
-    st.markdown("# Contagious Coughs & Colds 2019 Commute Calculator")
-    st.markdown("""An under-researched statistical toy \** to 
+    st.markdown("## Contagious Coughs & Colds 2019 Commute Calculator")
+    st.markdown("""An under-researched** statistical toy to 
                 estimate the 
                 probability of encountering someone with a contagious cough or 
                 cold during your daily round trip commute into the city.""")
 
     st.markdown('### A naive population resampling model')
 
-    st.markdown("""The probability $P(X>0)$ of a finding at least $1$ case in a 
-                group of $N$ people given a background no. of cases $m >> 1$ 
+    st.markdown("""The probability $P(X>0)$ of finding at least $1$ case in a 
+                group of $N$ people given a background no. of cases $m \gg 1$ 
                 in the local city population $M$ is a resample with replacement: """)
 
     st.sidebar.markdown('## Settings')
@@ -23,7 +23,6 @@ def main():
                             max_value=20000000, value=8500000, step=500000)
 
     p = m/M
-    #st.sidebar.markdown(f'Cases in population $p = {p*100:.3f}\%$')
 
     st.sidebar.markdown('### Commute Details')
 
@@ -36,7 +35,8 @@ def main():
                             list(commute_people_per_min.keys()), index=2)
     n_commuters = commute_people_per_min[commute_method]
 
-    st.sidebar.markdown(f"""<sub><sup>This method assumes aerosol contact ($\Delta \ 2m$) with 
+    st.sidebar.markdown(f"""<sub><sup>This method assumes aerosol contact 
+                ($\Delta \ 2m$) with 
                 ${n_commuters:.0f}$ new commuters per min during commute, 
                 inc. busy stations, crowded streets etc.</sup></sub>""", 
                 unsafe_allow_html=True)
@@ -46,31 +46,28 @@ def main():
 
     N = commute_people_per_min[commute_method] * commute_duration * 2
 
-    # st.sidebar.markdown(f'$N \\approx$ {N:.0f} commuters encountered during ' + 
-    #                     'a round trip commute')
-
-    #st.sidebar.markdown("Now view your $P(X>0) \\Longrightarrow$")
-
-
     st.latex(r'P(X>0) = 1 - (1-p)^{N} = f, \, \text{ ... where } \, p = \frac{m}{M}')
 
-    st.markdown("""$\\Longleftarrow$ _Setup the sliders on the left sidebar 
+    st.markdown("""$\\Longleftarrow$ _Setup the sliders on the left sidebar (**>**)
                 according to your situation_""")
 
     f = 1 - (1-(p))**N
 
     st.markdown('#### $\\Longrightarrow$ Results:')
 
-    st.info(f"""Proportion of cases in population $p = {p*100:.3f}\%$ 
-            \nCommuters encountered during a round trip commute $N \\approx$ {N:.0f}
-            \nYour est. prob. of encountering a contagious cough or cold during 
-             the daily round trip commute is: $P(X>0) = {f*100:.1f}\\%$""")
+    st.info(f"""Proportion of $m={m:.0f}$ cases in $M = {M/1e6:.1f}M$ pop.: $p = {p*100:.4f}\%$ 
+            \nCommuters encountered during a round trip commute: $N \\approx {N:.0f}$
+            \nEst. prob. of encountering a contagious cough or cold: $P(X>0) = {f*100:.1f}\%$""")
     
-    f5 = 1 - (1-f)**5
-    days= 'days'
-
     st.info(f"""... and over a 5 day week: $P(X>0|5 \, days) 
-                = 1 - (1-f)^5 = {f5*100:.1f}\\%$""")
+                = 1 - (1-f)^5 = {(1 - (1-f)**5) *100:.1f}\\%$""")
+
+    st.markdown("""Additional hack for Conference attendance: change the sliders
+                to generate $p$ appropriate to the sum of the expected attendees' 
+                home towns / states / countries and set $N$ appropriate to the 
+                number of conference attendees. Now $P(X>0)$ can help you decide 
+                whether to attend.""")
+
 
     st.markdown('---')
     st.markdown("""<sub>\** NOTE: This naive model is only to illustrate the power 
